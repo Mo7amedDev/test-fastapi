5a8d1cb7e8c85835964ad6c7d0b51fa076fd53a9
Mo7amedDev <medhasnaoui833@gmail.com>
2025-12-22 20:23:40 +0100

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    message = os.getenv("APP_MESSAGE", "Hello FastAPI!")
+    return {
+        "app": "sample-fastapi",
+        "message": message
+    }
